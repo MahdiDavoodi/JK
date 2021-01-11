667b46cb4b2ad321c6e9ae924056149c42aa0747
Mahdi Davoodi <70657577+MahdiDavoodi@users.noreply.github.com>
2021-01-11 22:47:59 +0300

    Update Solution.java

--- Learning Examples/HackerRank/Java & Problem Solving/Day 2: Operators/Solution.java
+++ Learning Examples/HackerRank/Java & Problem Solving/Day 2: Operators/Solution.java
@@ -3,12 +3,13 @@ import java.util.*;
 public class Solution {
 
     static void solve(double mealCost, int tipPercent, int taxPercent) {
-        int totalCost = (int) (mealCost
+        double totalCost = (mealCost
                 + ((mealCost * tipPercent) / 100)
                 + ((mealCost * taxPercent) / 100));
-        System.out.println(totalCost);
+        System.out.println(Math.round(totalCost));
 
     }
+
     private static final Scanner scanner = new Scanner(System.in);
 
     public static void main(String[] args) {
