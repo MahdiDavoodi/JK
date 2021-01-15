bd55cdb413abf4fb415fa64c1e369bf107f446df
Mahdi Davoodi <70657577+MahdiDavoodi@users.noreply.github.com>
2021-01-15 15:44:44 +0300

    Create Solution.java

---
+++ Learning Examples/HackerRank/Java & Problem Solving/Day 5: Loops/Solution.java
@@ -0,0 +1,17 @@
+import java.util.*;
+
+public class Solution {
+
+
+    private static final Scanner scanner = new Scanner(System.in);
+
+    public static void main(String[] args) {
+        int n = scanner.nextInt();
+        scanner.skip("(\r\n|[\n\r\u2028\u2029\u0085])?");
+        scanner.close();
+
+        for (int i = 1; i <= 10; i++) {
+            System.out.println(n + " x " + i + " = " + (n * i));
+        }
+    }
+}
