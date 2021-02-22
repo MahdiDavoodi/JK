d4a55a79520050580e0f940abc60c79b9365d9fb
Mahdi Davoodi <70657577+MahdiDavoodi@users.noreply.github.com>
2021-02-22 21:45:15 +0300

    Add files via upload

---
+++ Exercises/2021/February/IntegerArrayCollection.java
@@ -0,0 +1,38 @@
+package ir.blog.mahdidavoodi;
+
+public class IntegerArrayCollection {
+    private final IntegerSortedLinkedList[] dataBase;
+
+    public IntegerArrayCollection() {
+        dataBase = new IntegerSortedLinkedList[10];
+        for (int i = 0; i < 10; i++) {
+            dataBase[i] = new IntegerSortedLinkedList();
+        }
+    }
+
+    public void add(int element) {
+        dataBase[element % 10].add(element);
+    }
+
+    public int get(int i, int j) {
+        return dataBase[i].get(j);
+    }
+
+
+    public boolean contains(int element) {
+        return dataBase[element % 10].contains(element);
+    }
+
+    public void remove(int i, int j) {
+        dataBase[i].remove(j);
+    }
+
+    public void print() {
+        for (int i = 0; i < 10; i++) {
+            for (int j = 0; j < dataBase[i].getSize(); j++) {
+                System.out.format("%5d", dataBase[i].get(j));
+            }
+            System.out.println();
+        }
+    }
+}
