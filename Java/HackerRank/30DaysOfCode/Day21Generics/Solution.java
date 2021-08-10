fa5f5239af193d95372c51135f863e7845ad6015
Mahdi Davoodi <mahdidavoodi@gmail.com>
2021-08-10 06:21:13 +0300

    Day 21: Generics.

---
+++ Java/HackerRank/30DaysOfCode/Day21Generics/Solution.java
@@ -0,0 +1,43 @@
+package mahdi.davoodi;
+
+/*
+ * (Solved by me.)
+ * Link --->  https://www.hackerrank.com/challenges/30-generics/problem?h_r=profile
+ * */
+
+import java.util.*;
+
+class Printer<T> {
+    public void printArray(T[] array) {
+        for (T element :
+                array) {
+            System.out.println(element);
+        }
+    }
+}
+
+class Generics {
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int n = scanner.nextInt();
+        Integer[] intArray = new Integer[n];
+        for (int i = 0; i < n; i++) {
+            intArray[i] = scanner.nextInt();
+        }
+
+        n = scanner.nextInt();
+        String[] stringArray = new String[n];
+        for (int i = 0; i < n; i++) {
+            stringArray[i] = scanner.next();
+        }
+
+        Printer<Integer> intPrinter = new Printer<>();
+        Printer<String> stringPrinter = new Printer<>();
+        intPrinter.printArray(intArray);
+        stringPrinter.printArray(stringArray);
+        if (Printer.class.getDeclaredMethods().length > 1) {
+            System.out.println("The Printer class should only have 1 method named printArray.");
+        }
+    }
+}
