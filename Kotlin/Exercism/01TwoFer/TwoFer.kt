79eb95747efd57ef26007e98e0a25871f1d8db56
Mahdi Davoodi <mahdidavoodi@gmail.com>
2021-09-09 14:09:50 +0300

    Started exercism's Kotlin track!

---
+++ Kotlin/Exercism/01TwoFer/TwoFer.kt
@@ -0,0 +1,5 @@
+/*
+* A challenge by Exercism in Kotlin.
+* Link ---> https://exercism.org/tracks/kotlin/exercises/two-fer
+* */
+internal fun twofer(name: String = "you"): String = "One for $name, one for me."
